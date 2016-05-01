e24dc16127fd999e7a6e5c66f162f146d8d83eda
Mohamed Kamal <mak20202000@yahoo.com>
2016-05-01 13:32:40 +0300

    Update 231C - To Add or Not to Add.py

--- codeforces/231C - To Add or Not to Add.py
+++ codeforces/231C - To Add or Not to Add.py
@@ -9,7 +9,7 @@ for i in xrange(n):
     s[i + 1] = s[i] + ar[i]
 
 i, j, maxx, ans = 0, 1, 1, ar[0]
-for j in xrange(n):
+while j < n:
     while ((j - i) * ar[j] - s[j] + s[i]> k): i += 1
     if j - i + 1 > maxx:
     	maxx, ans = j - i + 1, ar[j]
